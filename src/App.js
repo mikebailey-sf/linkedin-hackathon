31bbcdda2d12e01f2839b0afc5e5a3b6aa315555
teknoformula <seraficaj@gmail.com>
2019-07-26 16:00:18 -0700

    basic app demo

--- src/App.js
+++ src/App.js
@@ -10,10 +10,61 @@ import Lonely from './components/Lonely';
 import data from './data.json'
 
 const App = () => {
+
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = useState([]);
+  const [superLikedUsers, setSuperLikedUsers] = useState([]);
+  const [dislikedUsers, setDislikedUsers] = useState([]);
+  const activeUser = 0;
+
+  const removedPersonFromDataSrc = (peopleSource, userId) =>
+    peopleSource.filter(user => user.id !== userId);
+
+  const modifyChoices = (userId, action) => {
+    const newPeople = [...people];
+    const newLikedUsers = [...likedUsers];
+    const newDislikedUsers = [...dislikedUsers];
+
+    switch (action) {
+      case 'ADD_TO_LIKED_USERS':
+        if (!people[activeUser].likedUsers.includes(userId)) {
+          newPeople[activeUser].likedUsers.push(userId);
+          newLikedUsers.push(data[userId]);
+
+          setLikedUsers(newLikedUsers);
+          setPeople(removedPersonFromDataSrc(people, userId));
+        }
+        break;
+      case 'ADD_TO_DISLIKED_USERS':
+        if (!people[activeUser].dislikedUsers.includes(userId)) {
+          newPeople[activeUser].dislikedUsers.push(userId);
+          newDislikedUsers.push(data[userId]);
+
+          setDislikedUsers(newLikedUsers);
+          setPeople(removedPersonFromDataSrc(people, userId));
+        }
+        break;
+      default:
+        return people;
+    }
+  };
+
   return (
   <div className="app">
     <Header />
-    
+    {people[1] ? (
+        <Person
+          key={people[1].id}
+          person={people[1]}
+          modifyChoices={modifyChoices}
+          likedUsers={likedUsers}
+        />
+      ) : (
+        <Lonely
+          activeUserImage={people[activeUser].image}
+          likedUsers={likedUsers}
+        />
+      )}
   </div>
   ) 
 }
--- src/components/LikedPerson.js
+++ src/components/LikedPerson.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 
 const LikedPerson = ({person}) => {
+  return (
   <div className="liked-person">
-    <div className="liked-person-image">
-      <img 
-        src={`/images/users/${person.image}`}
-        alt={`${person.name}`}
-      />
+      <div className="liked-person-image">
+        <img 
+          src={`/images/users/${person.image}`}
+          alt={`${person.name}`}
+        />
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default LikedPerson;
