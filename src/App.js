5433384cb47b0a11c2727a6434f9294b1e7a1c32
Mike <mikeyobviously@gmail.com>
2019-07-27 10:58:29 -0700

    auth

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super();
     this.state = {
       code: null,
-      accesstoken: AQVZ74UBvd1kZ9DeL3BqGnyu5tCmW8BJcLioJ31GD3EfsOyrHat0VnK24WmTdKMRg-W6RX8vCvUGSyMo5Vl6151PK95sTULiXifaD_V3Yir4qhDNqAuLKso9FnsumDmmM2eLMlQv-0J8lowL_KbCakn2sOWoOB4LN1x4ppVVpMw3kFOcpqFz9Sw2ZU2qsdoYgWUdZMMf-SB7KEE8uTJJAOqiYnotfaV5D8-4ZhYkVGPptDzimMG4wT-aJ9Tcm1TDId9jfUa1VMIPlLBQejtsG_Tj2CzlRFB6jpCb2HgIIIzs5cXNXtbcjL6a-nGfK5SH2yQZWy9cE0485EOEJI_ud_GR6MvmBg
+      accesstoken: 'AQVZ74UBvd1kZ9DeL3BqGnyu5tCmW8BJcLioJ31GD3EfsOyrHat0VnK24WmTdKMRg-W6RX8vCvUGSyMo5Vl6151PK95sTULiXifaD_V3Yir4qhDNqAuLKso9FnsumDmmM2eLMlQv-0J8lowL_KbCakn2sOWoOB4LN1x4ppVVpMw3kFOcpqFz9Sw2ZU2qsdoYgWUdZMMf-SB7KEE8uTJJAOqiYnotfaV5D8-4ZhYkVGPptDzimMG4wT-aJ9Tcm1TDId9jfUa1VMIPlLBQejtsG_Tj2CzlRFB6jpCb2HgIIIzs5cXNXtbcjL6a-nGfK5SH2yQZWy9cE0485EOEJI_ud_GR6MvmBg'
     };
   }
 
@@ -20,9 +20,9 @@ class App extends Component {
     let params = new URLSearchParams(search);
     let code = params.get('code');
     this.setState({code});
-
+    this.getUserInfo();
     if (code) {
-      this.exchangeToken(code);
+      //this.exchangeToken(code);
     }
   }
 
@@ -38,6 +38,21 @@ class App extends Component {
       //this.setState({accesstoken});
     });
   }
+
+  getUserInfo(){
+    return fetch('https://api.linkedin.com/v2/me', {
+      method: 'GET',
+      mode: 'no-cors',      
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': 'AQVZ74UBvd1kZ9DeL3BqGnyu5tCmW8BJcLioJ31GD3EfsOyrHat0VnK24WmTdKMRg-W6RX8vCvUGSyMo5Vl6151PK95sTULiXifaD_V3Yir4qhDNqAuLKso9FnsumDmmM2eLMlQv-0J8lowL_KbCakn2sOWoOB4LN1x4ppVVpMw3kFOcpqFz9Sw2ZU2qsdoYgWUdZMMf-SB7KEE8uTJJAOqiYnotfaV5D8-4ZhYkVGPptDzimMG4wT-aJ9Tcm1TDId9jfUa1VMIPlLBQejtsG_Tj2CzlRFB6jpCb2HgIIIzs5cXNXtbcjL6a-nGfK5SH2yQZWy9cE0485EOEJI_ud_GR6MvmBg'
+      }),
+      //body: `grant_type=authorization_code&code=${code}&redirect_uri=http://localhost:3000/&client_id=78dwkxwigx6ii5&client_secret=edivLBOAkw7aWb0E`
+    }).then(response => {
+      console.log(JSON.stringify(response));
+      //this.setState({accesstoken});
+    });      
+  }  
 
   render() {
     return(
